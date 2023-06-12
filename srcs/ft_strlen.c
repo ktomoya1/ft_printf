d3985ec6864fcd5106b5ee12b8861b95cb6c5d8d
ktomoya <twbtomoya2@gmail.com>
2023-06-12 15:53:31 +0900

    change type from size_t to ssize_t

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -6,15 +6,15 @@
 /*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 11:40:29 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/05/19 13:34:20 by ktomoya          ###   ########.fr       */
+/*   Updated: 2023/06/12 15:52:43 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+ssize_t	ft_strlen(const char *s)
 {
-	size_t	count;
+	ssize_t	count;
 
 	count = 0;
 	while (s[count] != '\0')
