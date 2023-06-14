4ebca429fc564f93ff2d058d4b818a7f6e5dcb9a
ktomoya <twbtomoya2@gmail.com>
2023-06-14 18:48:20 +0900

    delete base_is_validate func

--- ft_putnbr_base_set.c
+++ ft_putnbr_base_set.c
@@ -6,36 +6,11 @@
 /*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 20:09:58 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/06/14 17:09:29 by ktomoya          ###   ########.fr       */
+/*   Updated: 2023/06/14 18:47:13 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-static int	base_is_validate(char *base)
-{
-	int	i;
-	int	search;
-
-	if (ft_strlen(base) == 0 || ft_strlen(base) == 1)
-		return (0);
-	i = 0;
-	while (base[i] != '\0')
-	{
-		if (base[i] == '+' || base[i] == '-'
-			|| !(' ' <= base[i] && base[i] <= '~'))
-			return (0);
-		search = 0;
-		while (search < i)
-		{
-			if (base[i] == base[search])
-				return (0);
-			search++;
-		}
-		i++;
-	}
-	return (1);
-}
 
 ssize_t	put_ptr_fd(uintptr_t hexnbr, char *base, int fd)
 {
@@ -43,11 +18,7 @@ ssize_t	put_ptr_fd(uintptr_t hexnbr, char *base, int fd)
 	ssize_t		chars;
 
 	chars = 0;
-	if (base == NULL)
-		return (0);
 	base_len = ft_strlen(base);
-	if (!base_is_validate(base))
-		return (0);
 	if (hexnbr >= base_len)
 		chars = put_ptr_fd(hexnbr / base_len, base, fd);
 	chars += write(fd, &base[hexnbr % base_len], 1);
@@ -60,11 +31,7 @@ ssize_t	put_uint_fd(unsigned int nbr, char *base, int fd)
 	ssize_t		chars;
 
 	chars = 0;
-	if (base == NULL)
-		return (0);
 	base_len = ft_strlen(base);
-	if (!base_is_validate(base))
-		return (0);
 	if (nbr >= base_len)
 		chars = put_uint_fd(nbr / base_len, base, fd);
 	chars += write(fd, &base[nbr % base_len], 1);
@@ -77,11 +44,7 @@ ssize_t	put_int_fd(int nbr, char *base, int fd)
 	ssize_t	chars;
 
 	chars = 0;
-	if (base == NULL)
-		return (0);
 	base_len = ft_strlen(base);
-	if (!base_is_validate(base))
-		return (0);
 	if (nbr == -2147483648)
 	{
 		chars = write(fd, "-2147483648", 11);
